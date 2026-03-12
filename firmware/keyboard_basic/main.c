39d4ebf63413811d29ea6fbdd163aa681efb6e19
Erik Bosman <erik@minemu.org>
2026-03-12 18:47:21 +0100

    correct copyright date

--- firmware/keyboard_basic/main.c
+++ firmware/keyboard_basic/main.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Erik Bosman <erik@minemu.org>
+ * Copyright (c) 2026 Erik Bosman <erik@minemu.org>
  *
  * Permission  is  hereby  granted,  free  of  charge,  to  any  person
  * obtaining  a copy  of  this  software  and  associated documentation
